cb4ee6a06f4657370bc4e3d2359baa8e782417bf
kghyun <gg12_s0@naver.com>
2021-07-13 14:35:32 +0900

    login, userID Duplicate update

--- src/main/java/com/laonstory/todo/controller/UserController.java
+++ src/main/java/com/laonstory/todo/controller/UserController.java
@@ -5,9 +5,13 @@ import com.laonstory.todo.dto.UserDto;
 import com.laonstory.todo.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/api")
@@ -15,18 +19,37 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    /*
-    @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody User user){
-        return userService.login(user);
+    @GetMapping("/login")
+    public ResponseEntity<UserDto> login(@RequestBody UserDto userDto, HttpServletRequest req){
+        System.out.println("login start");
+        HttpSession session = req.getSession();
+        UserDto login = userService.login(userDto);
+        if(login == null){
+            session.setAttribute("user",null);
+            return ResponseEntity.ok(null);
+        }else {
+            session.setAttribute("user", login.getUserID());
+            return ResponseEntity.ok(userDto);
+        }
     }
-    */
 
     @PostMapping("/signup")
-    public void SignUp(@RequestBody User user){
+    public boolean SignUp(@RequestBody UserDto userDto){
+        System.out.println("join start");
         //중복되는 경우 true
-        if(!userService.ckeckUserIDDuplicate(user.getUserID())){
-            userService.signup(user);
+        if(!userService.ckeckUserIDDuplicate(userDto.getUserID())){
+            userService.signup(userDto);
+            System.out.println("join done");
+            return true;
         }
+        System.out.println("join failed");
+        return false;
+    }
+
+    //아이디 중복 검사(true/false) 버튼
+    @GetMapping("/duplicateTest")
+    public ResponseEntity<Boolean> checkUserIDDuplicate(@RequestBody String userID){
+        System.out.println("userID Duplicate start");
+        return ResponseEntity.ok(userService.ckeckUserIDDuplicate(userID));
     }
 }
--- src/main/java/com/laonstory/todo/service/UserService.java
+++ src/main/java/com/laonstory/todo/service/UserService.java
@@ -2,6 +2,7 @@ package com.laonstory.todo.service;
 
 import com.laonstory.todo.domain.entity.User;
 import com.laonstory.todo.domain.repository.UserRepository;
+import com.laonstory.todo.dto.UserDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
@@ -12,8 +13,13 @@ public class UserService{
     private UserRepository userRepository;
 
     @Transactional
-    public User signup(User user){
-        return userRepository.save(user);
+    public UserDto login(UserDto userDto){
+        return userRepository.findByUserIDAndPassword(userDto.getUserID(), userDto.getPassword());
+    }
+
+    @Transactional
+    public void signup(UserDto userDto){
+        userRepository.save(userDto.toEntity());
     }
 
     @Transactional
