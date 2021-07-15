b25224d18b37ffb1cf8cc3ccb976d233a80aea82
kghyun <gg12_s0@naver.com>
2021-07-15 19:44:46 +0900

    CRUD 최종

--- src/main/java/com/laonstory/todo/domain/repository/TodoRepository.java
+++ src/main/java/com/laonstory/todo/domain/repository/TodoRepository.java
@@ -3,17 +3,19 @@ package com.laonstory.todo.domain.repository;
 import com.laonstory.todo.domain.entity.Todo;
 import com.laonstory.todo.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Integer> {
-    @Query(value = "select count(*)+1 from Todo t where t.userID = ?1" )
+    @Query(value = "select count(*) from Todo t where t.userID = ?1" )
     public Integer countByUserID(String userID);
     public List<Todo> findByUserID(String userID);
     public Todo findByListNumAndUserID(Integer listNum, String userID);
-    @Query(value = "SET @CNT = 0; UPDATE Todo t SET t.list_num = @CNT:=@CNT+1", nativeQuery=true)
-    public void resetByuserID(String userID);
+    @Query(value = "select max(t.listNum) from Todo t where t.userID = ?1" )
+    public Integer MaxByUserID(String userID);
 }
