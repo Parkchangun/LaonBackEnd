670da819d1b21048dc11df81cecb5566416a7bb5
kghyun <gg12_s0@naver.com>
2021-07-15 13:47:56 +0900

    update, delete(post) 수정

--- src/main/java/com/laonstory/todo/controller/TodoController.java
+++ src/main/java/com/laonstory/todo/controller/TodoController.java
@@ -18,14 +18,15 @@ public class TodoController {
 
     @PostMapping("/todo") //모든 list 출력
     public ResponseEntity<List<TodoDto>> list(@RequestBody String userID){
+        System.out.println("read");
         userID = userID.replace("=", "");
         List<TodoDto> todoDtoList = todoService.getTodoList(userID);
-        System.out.println(todoDtoList);
         return ResponseEntity.ok(todoDtoList);
     }
 
     @PostMapping("/post") //입력 후 list 반환
     public ResponseEntity<List<TodoDto>> post(@RequestBody TodoDto todoDto){
+        System.out.println("create");
         Integer num = todoService.countUserIDPost(todoDto.getUserID());
         todoDto.setListNum(num);
         todoDto.setDone(false);
@@ -38,8 +39,9 @@ public class TodoController {
         }
     }
   
-    @PutMapping("/update")
+    @PostMapping("/update")
     public ResponseEntity<List<TodoDto>> update(@RequestBody TodoDto todoDto){
+        System.out.println("update");
         if(todoService.update(todoDto)){
             List<TodoDto> todoDtoList = todoService.getTodoList(todoDto.getUserID());
             System.out.println(todoDtoList);
@@ -49,12 +51,11 @@ public class TodoController {
         }
     }
 
-    @DeleteMapping("/delete")
+    @PostMapping("/delete")
     public ResponseEntity<Boolean> delete(@RequestBody Integer listNum, @RequestBody String userID){
+        System.out.println("0");
         return ResponseEntity.ok(todoService.delete(listNum, userID));
     }
 
-
-    
 }
 
